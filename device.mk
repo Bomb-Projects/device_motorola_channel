73222e8dc901b427aa807f3166459ba291f210b7
Vhmit <viktor.9630@protonmail.com>
2024-09-10 21:51:23 -0300

    channel: Switch to EroFS * We are no longer able to use retrofits after the arrival of QPR2.
    Change-Id: I355f1b01750cbaacbc03a5572723fd70286cf25e
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 # Audio
